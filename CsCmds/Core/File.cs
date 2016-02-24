514f48ee95fbcee4f175bb785287f920df4ffb1b
hal1932 <yu.arai.19@gmail.com>
2016-02-24 20:34:15 +0900

    リファレンスの実装を整理

--- CsCmds/Core/File.cs
+++ CsCmds/Core/File.cs
@@ -46,41 +46,5 @@ namespace CsCmds.Core
             MFileIO.importFile(filepath.AsNormalizedPath(), type, preserveReference, nameSpace, ignoreVersion);
         }
         #endregion
-
-        #region reference
-        public class Reference : DependNode
-        {
-            public string Name { get { return FnDependNode.name; } }
-
-            private Reference(MObject obj, MFnDependencyNode fn)
-                : base(obj, fn)
-            { }
-
-            public static IEnumerable<Reference> Enumerate()
-            {
-                var iter = new MItDependencyNodes(MFn.Type.kReference);
-                while (!iter.isDone)
-                {
-                    yield return new Reference(iter.item, null);
-                    iter.next();
-                }
-            }
-
-            public string GetFileName()
-            {
-                return MFileIO.getReferenceFileByNode(MObject);
-            }
-
-            public string GetFullName()
-            {
-                return MGlobal.executeCommandStringResult("referenceQuery -filename " + Name);
-            }
-
-            public string GetNameSpace()
-            {
-                return MGlobal.executeCommandStringResult("referenceQuery -namespace " + Name);
-            }
-        }
-        #endregion
     }
 }
---
+++ CsCmds/Core/Reference.cs
@@ -0,0 +1,50 @@
+﻿using Autodesk.Maya.OpenMaya;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsCmds.Core
+{
+    public class Reference : DependNode
+    {
+        public string NameSpace { get { return FnDependNode.name; } }
+
+        private Reference(MObject obj, MFnDependencyNode fn)
+            : base(obj, fn)
+        { }
+
+        public static IEnumerable<Reference> Enumerate(Func<string, bool> nameSpaceFilter = null)
+        {
+            var iter = new MItDependencyNodes(MFn.Type.kReference);
+            while (!iter.isDone)
+            {
+                if (nameSpaceFilter != null)
+                {
+                    var fn = new MFnDependencyNode(iter.item);
+                    if (fn.name.IsFilterd(nameSpaceFilter))
+                    {
+                        continue;
+                    }
+                    yield return new Reference(iter.item, fn);
+                }
+                else
+                {
+                    yield return new Reference(iter.item, null);
+                }
+                iter.next();
+            }
+        }
+
+        public string GetFileName()
+        {
+            return MFileIO.getReferenceFileByNode(MObject);
+        }
+
+        public string GetFullName()
+        {
+            return MGlobal.executeCommandStringResult("referenceQuery -filename " + NameSpace);
+        }
+    }
+}
