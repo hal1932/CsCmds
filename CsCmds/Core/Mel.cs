fffac63ba1f141060cef9b264e4728c658f62a7f
hal1932 <yu.arai.19@gmail.com>
2016-02-28 16:22:08 +0900

    Maya2014で落ちる問題を回避

--- CsCmds/Core/Mel.cs
+++ CsCmds/Core/Mel.cs
@@ -6,6 +6,11 @@ namespace CsCmds.Core
     {
         public static string Evaluate(string command, bool displayEnabled = true, bool undoEnabled = true)
         {
+            // HACK: thw workaround for the problem that Maya2014 throws a System.ApplicationException when you use the executeCommand(string, bool, bool)
+            if (MGlobal.mayaVersion.StartsWith("2014"))
+            {
+                return MGlobal.executeCommandStringResult(command, displayEnabled);
+            }
             return MGlobal.executeCommandStringResult(command, displayEnabled, undoEnabled);
         }
 
