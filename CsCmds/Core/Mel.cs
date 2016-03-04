44061ef8fc7a2b56336fea34597403a641dadeeb
hal1932 <yu.arai.19@gmail.com>
2016-03-04 23:11:42 +0900

    mel.evalがResultをそのまま返すように

--- CsCmds/Core/Mel.cs
+++ CsCmds/Core/Mel.cs
@@ -4,19 +4,21 @@ namespace CsCmds.Core
 {
     public static class Mel
     {
-        public static string Evaluate(string command, bool displayEnabled = true, bool undoEnabled = true)
+        public static MCommandResult Evaluate(string command, bool displayEnabled = true, bool undoEnabled = true)
         {
-            // HACK: thw workaround for the problem that Maya2014 throws a System.ApplicationException when you use the executeCommand(string, bool, bool)
-            if (MGlobal.mayaVersion.StartsWith("2014"))
-            {
-                return MGlobal.executeCommandStringResult(command, displayEnabled);
-            }
-            return MGlobal.executeCommandStringResult(command, displayEnabled, undoEnabled);
+            var result = new MCommandResult();
+            MGlobal.executeCommand(command, result, displayEnabled, undoEnabled);
+            return result;
         }
 
         public static void Evaluate(string command, MDGModifier modifier)
         {
             modifier.commandToExecute(command);
+        }
+
+        public static void EvaluateFile(string filepath)
+        {
+            MGlobal.sourceFile(filepath.AsNormalizedPath());
         }
 
         public static void EvaluateOnIdle(string command, bool displayEnabled = true)
---
+++ CsCmds/extensions/MCommandResultExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.Maya.OpenMaya;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsCmds.extensions
+{
+    public static class MCommandResultExtensions
+    {
+        public static T GetResult<T>(this MCommandResult result)
+        {
+            var type = typeof(T);
+            T value = default(T);
+
+            if (type == typeof(int)) result.getResult((out int)Convert.ChangeType(out value, typeof(out int)));
+            else if (type == typeof(double)) result.getResult(out value);
+
+            return result;
+        }
+
+        private static void CopyTypedResult<T, U>(out T outValue, U value)
+        {
+            outValue = (T)Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
